249b1f07dd298bb2f401f3855bad749dab0afe2d
cejoji <bensonfran@gmail.com>
2025-06-30 21:45:13 +0200

    Add files via upload

---
+++ components/CejojiAI.js
@@ -0,0 +1,7 @@
+export default function CejojiAI() {
+  return (
+    <div className="fixed bottom-4 right-4 bg-white border p-4 shadow-lg rounded">
+      <p>Cejoji.AI Assistant Coming Soon!</p>
+    </div>
+  );
+}
